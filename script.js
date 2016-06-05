a5cafc416db3f882c6ee6b2dc8a5c97f156b303b
Jordan Enev <jordantoen@gmail.com>
2016-06-06 00:06:37 +0300

    Add options for splitting by groups and chunks.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var Library = function(array, options){
 	 * Randomize array element order in-place.
 	 * Using Durstenfeld shuffle algorithm.
 	 */
-	var shuffleArray =  function() {
+	var shuffleArray = function() {
 		for (var i = array.length - 1; i > 0; i--) {
 			var j = Math.floor(Math.random() * (i + 1));
 			var temp = array[i];
@@ -14,9 +14,9 @@ var Library = function(array, options){
 		}
 	};
 
-	var groupArray = function() {
+	var splitArrayByChunks = function() {
 		var i,j,group;
-		var chunk = options.chunk;
+		var chunk = options.chunks;
 
 		for (i=0,j=array.length; i<j; i+=chunk) {
 			group = array.slice(i,i+chunk);
@@ -24,20 +24,31 @@ var Library = function(array, options){
 		}
 	};
 
+	var splitArrayByGroups = function() {
+		var groups = options.groups;
+		options.chunks = array.length / groups;
+		splitArrayByChunks();
+	};
+
 	var validate = function() {
 		if ( ! options ) {
 			options = {};
 		}
 
-		if ( ! options.chunk ) {
-			options.chunk = array.length;
+		if ( ! (options.chunks || options.groups)) {
+			options.chunks = array.length;
 		}
 	};
 
-	this.randomGrouping = function(array, options) {
+	this.randomGrouping = function() {
 		validate();
 		shuffleArray();
-		groupArray();
+
+		if (options.chunks) {
+			splitArrayByChunks();
+		} else {
+			splitArrayByGroups();
+		}
 
 		return groups;
 	}
